36bc273e9ef24d9ee755375bcdc6801aadf538be
shockeyt <shockeyt@gmail.com>
2017-04-25 22:35:25 -0600

    finished the challenges

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,31 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("ExerciseController", ExerciseController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+  this.full_name = "Tyler Shockey";
+  this.age = 32;
+  this.city = "Ardmore";
+  this.state = "Oklahoma";
+  this.shout = function() {
+  	return this.full_name.toUpperCase() + "!";
+  };
+
+}
+
+function ExerciseController(){
+	console.log("exercise controller working");
+	this.class_name = "WDI3";
+	this.enrolled_students = 12;
+	this.start_date = "03/13/2017";
+	this.end_date = "06/03/2017";
+	this.daysRemaining = function() {
+		this.start_date = new Date("03/13/2017");
+		this.end_date = new Date("06/03/2017");
+		var timeDiff = Math.abs(this.end_date.getTime() - this.start_date.getTime());
+		var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+		return diffDays;
+	};
+}
--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,69 @@
 console.log('challenge #3 -- app.js loaded!');
 
-// var app = angular.module("appThree", []);
-// app.controller("infoController", infoController)
+var app = angular.module("appThree", []);
+app.controller("infoController", infoController)
+app.controller("loanController", loanController)
 
-// function infoController(){
-//   this.name = "Jon Doe"; // default name
-//   this.height = 5.6; // default height in feet
-// }
+function infoController(){
+  this.name = "Jon Doe"; // default name
+  this.height = 5.6; // default height in feet
+}
+
+function loanController(){
+	this.amount = 1000;
+	this.apr = 9;
+
+	//this.total_balance = interest + this.amount;
+	//this.accrued_interest = interest;
+
+	this.month = function(n) {
+		var year = this.amount * (this.apr / 100);
+		var month = (year / 12);
+		var round = Math.round(month * 100) / 100;
+		//var round = month.toFixed(2);
+		var interest = n * round;
+		return {
+			accrued_interest: interest,
+			total_balance: this.amount + interest
+		};
+
+	};
+
+
+
+	// this.month = function(n) {
+	// 	if (n == 1) {
+	// 		console.log("month 1");
+	// 		this.total_balance = function() {
+	// 		var interest = this.amount * this.apr;
+	// 		console.log(this.amount + interest);
+	// 		return this.amount + interest;
+	// 		};
+	// 		this.accrued_interest = function() {
+	// 		var accrue = this.amount * this.apr;
+	// 		return accrue;
+	// 		};
+	// 	}
+	// 	if (n == 2) {
+	// 		this.total_balance = function() {
+	// 		var interest = this.amount * this.apr;
+	// 		return (this.amount + interest) * 2;
+	// 		};
+	// 		this.accrued_interest = function() {
+	// 		var accrue = this.amount * this.apr;
+	// 		return accrue * 2;
+	// 		};
+	// 	}
+	// 	if (n == 12) {
+	// 		this.total_balance = function() {
+	// 		var interest = this.amount * this.apr;
+	// 		return (this.amount + interest) * 12;
+	// 		};
+	// 		this.accrued_interest = function() {
+	// 		var accrue = this.amount * this.apr;
+	// 		return accrue * 12;
+	// 		};
+	// 	}
+	// };
+	
+}
